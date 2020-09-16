6939508e0aa62ce0a85aad90d8fa04973d308601
sato-kodai <satoverphone@gmail.com>
2020-09-16 10:02:05 +0900

    deviseに関するビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   private
   def basic_auth
-    authenticate_or_request_with_http_basic do |email, password|
-      email == ENV["BASIC_AUTH_EMAIL"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
