541e1591a40763e839eb3f1ca3e3c363aa74c853
sato-kodai <satoverphone@gmail.com>
2020-09-16 14:39:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i },
+                      uniqueness: true
+                      #@のチェックコードは記載しなくても、エラーのレスポンスが表示される
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
 end
