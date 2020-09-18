0f01b610e230974e5e53b1777c753394049190c4
sato-kodai <satoverphone@gmail.com>
2020-09-18 21:07:27 +0900

    配送料負担のAcitveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status
+  belongs_to_active_hash :category, :status, :shipping_cost
   has_one_attached :image
 
 end
