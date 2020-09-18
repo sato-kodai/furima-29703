98a6a870184178546b76b23c9df62470eaea69a5
sato-kodai <satoverphone@gmail.com>
2020-09-18 13:25:12 +0900

    コードレビューによる修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
-    email                 { Faker::Internet.free_email }
+    email                 { Faker::Internet.email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name             { '山田' }
