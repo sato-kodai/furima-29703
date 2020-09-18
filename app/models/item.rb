aba20a78f216ca22e44d6f61b8c1c91e8f63bcd4
sato-kodai <satoverphone@gmail.com>
2020-09-18 21:30:44 +0900

    発送元の地域のActiveHashを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :shipping_cost
+  belongs_to_active_hash :category, :status, :shipping_cost, :shipment_source
   has_one_attached :image
 
 end
