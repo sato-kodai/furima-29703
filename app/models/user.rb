e76eab26e4281563ba861c811e2c3ba5dafb0dd0
sato-kodai <satoverphone@gmail.com>
2020-09-18 14:32:53 +0900

    正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  Name_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  Name_Kana_REGEX = /\A[ァ-ン]+\z/.freeze
 
   with_options presence: true do
     validates :nickname
@@ -10,10 +13,10 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: true }
     # @のチェックコードは記載しなくても、エラーのレスポンスが表示される
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :last_name, format: { with: Name_REGEX }
+    validates :first_name, format: { with: Name_REGEX }
+    validates :last_name_kana, format: { with: Name_Kana_REGEX }
+    validates :first_name_kana, format: { with: Name_Kana_REGEX }
     validates :birthday
   end
 end
