94ef6c3c864e9869b9622a08b63293b6f6435318
sato-kodai <satoverphone@gmail.com>
2020-09-18 19:01:13 +0900

    ststusのAciveHash実装＆テストファイル生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to_active_hash :category, :status
   has_one_attached :image
 
 end
