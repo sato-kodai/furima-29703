f122813820742a3a9fdcb02628cd26a068f8d352
sato-kodai <satoverphone@gmail.com>
2020-09-18 11:28:24 +0900

    バリデーションの修正＃3
    文字数制限の{6,0}の記載箇所修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                      uniqueness: { case_sensitive: true}
-                      #@のチェックコードは記載しなくても、エラーのレスポンスが表示される
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i }
+                      uniqueness: { case_sensitive: true }
+    # @のチェックコードは記載しなくても、エラーのレスポンスが表示される
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
     validates :birthday
   end
-
 end
