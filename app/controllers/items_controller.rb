debfe606152c236d25a9aa324a280da673508bd5
sato-kodai <satoverphone@gmail.com>
2020-09-18 17:02:20 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,10 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :comment, :category_id, :status_id, :shipping_cost_id, :shipment_source_id, :shipping_days_id, :price,:imgage).merge(user: currrent_user.id)
+  end
 end
