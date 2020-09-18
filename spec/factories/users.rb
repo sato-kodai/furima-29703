0f8102e9f7fbcc80c71998e39b4b625b630234db
sato-kodai <satoverphone@gmail.com>
2020-09-18 11:34:20 +0900

    userのFactoryBotの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"山田"}
-    first_name            {"リく太郎"}
-    last_name_kana        {"ヤマダ"}
-    first_name_kana       {"リクタロウ"}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { 'リく太郎' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'リクタロウ' }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
