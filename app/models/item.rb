8ebf4fd04857f6cb7276986beb6f88629c043b54
sato-kodai <satoverphone@gmail.com>
2020-09-19 10:17:37 +0900

    配送日数のActiveHashの実装＆テストファイル生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :shipping_cost, :shipment_source
+  belongs_to_active_hash :category, :status, :shipping_cost, :shipment_source, :shipping_day
   has_one_attached :image
 
 end
