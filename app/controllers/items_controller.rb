4afcb9068acf53f710904c29426da7fc3ca34b38
sato-kodai <satoverphone@gmail.com>
2020-09-19 23:01:51 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-
   def item_params
-    params.require(:item).permit(:name, :comment, :category_id, :status_id, :shipping_cost_id, :shipment_source_id, :shipping_days_id, :price,:imgage).merge(user: currrent_user.id)
+    params.require(:item).permit(:image, :name, :comment, :category_id, :status_id, :shipping_cost_id, :shipment_source_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,25 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :shipping_cost, :shipment_source, :shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :shipping_days
   has_one_attached :image
 
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :comment, length: { maximum: 1000 }
+    validates :price, format: {with: /\A[0-9]+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :shipment_source_id
+    validates :shipping_days_id
+  end
 end
