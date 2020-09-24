7869df0989bc443d69f7a5f11dccb77198f2b1ad
sato-kodai <satoverphone@gmail.com>
2020-09-24 20:58:25 +0900

    purchaseコントローラーの設定（クレカ決済＆fomオブジェクトによる保存）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,36 @@
 class PurchasesController < ApplicationController
+  
   def index
-    @item = Item.new(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase = PurchaseAddress.new(purchase_params)
+    if @purchase.valid?
+        pay_item
+        @purchase.save
+      return redirect_to root_path
+    else
+      redirect_to item_purchases_path(@purchase.item_id)
+    end
   end
 
+  private
+
+  def purchase_params
+    params.permit(:token, :item_id, :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def item_info
+    @item = Item.find(purchase_params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item_info[:price],
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
+  end
 end
