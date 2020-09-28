27d9848536c912e1757cc33efeafa1b9030d05cd
sato-kodai <satoverphone@gmail.com>
2020-09-29 08:33:32 +0900

    Capistranoの設定修正＃3

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29703'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sato-kodai/https://github.com/sato-kodai/furima-29703.git'
+set :repo_url,  'git@github.com:sato-kodai/furima-29703.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
